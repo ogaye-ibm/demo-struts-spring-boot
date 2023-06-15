1035383023da0430fbb88355abff3dbc3a978882
OGaye <gayeomar@hotmail.com>
2023-06-15 15:40:19 -0400

    Adding content

--- src/main/java/com/bofa/demo/struts/config/WebSecurityConfig.java
+++ src/main/java/com/bofa/demo/struts/config/WebSecurityConfig.java
@@ -1,17 +1,18 @@
 package com.bofa.demo.struts.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//@Configuration
+//@EnableWebSecurity
+public class WebSecurityConfig { //extends WebSecurityConfigurerAdapter {
 
-    @Override
+
+/*    @Override
     protected void configure(HttpSecurity http) throws Exception {
         System.out.println("   ***** Disabling CSRF ...");
         http.csrf().disable();
-    }
+    }*/
 }
